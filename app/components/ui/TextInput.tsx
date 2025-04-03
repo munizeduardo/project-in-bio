3ef52d3773d3a0bed831873c11ae5876f67f8b5c
Eduardo Muniz <thirteenwannabegood@gmail.com>
2025-04-02 23:53:59 -0300

    feat: TextInput component created

---
+++ app/components/ui/TextInput.tsx
@@ -0,0 +1,11 @@
+import { cn } from "@/app/lib/utils";
+
+export default function TextInput(props: React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <input {...props} className={cn(`
+      w-full bg-background-secondary text-white placeholder:text-content-placeholder rounded-xl border border-transparent hover:border-border-secondary hover:text-content-body active:border-border-tertiary
+      `,
+      props.className
+    )} />
+  )
+}
