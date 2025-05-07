b822f5d81990759246543fe4b9158b3735e06ad6
Eduardo Muniz <thirteenwannabegood@gmail.com>
2025-05-07 11:51:28 -0300

    feat: Header component creation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
+import Header from "./components/landing-page/Header";
 import Hero from "./components/landing-page/Hero";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
+      <Header />
       <Hero />
-      {/* <Header />
+      {/* 
       
       <VideoExplanation />
       <Pricing />
