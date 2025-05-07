2f69c76a5198487789d22094a824e64ed00a4df4
Eduardo Muniz <thirteenwannabegood@gmail.com>
2025-05-07 13:30:13 -0300

    feat: VideoExplanation component added (skeleton)

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,16 @@
 import Header from "./components/landing-page/Header";
 import Hero from "./components/landing-page/Hero";
+import VideoExplanation from "./components/landing-page/VideoExplanation";
 
 export default function Home() {
   return (
     <div className="max-w-7xl mx-auto">
       <Header />
       <Hero />
+      <VideoExplanation />
       {/* 
       
-      <VideoExplanation />
+      
       <Pricing />
       <FAQ /> */}
     </div>
