914855d5ec074e57df7c312a256b251a7d9fc806
Eduardo Muniz <thirteenwannabegood@gmail.com>
2025-05-07 13:54:32 -0300

    feat: Pricing component creation

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "./components/landing-page/Header";
 import Hero from "./components/landing-page/Hero";
+import Pricing from "./components/landing-page/Pricing";
 import VideoExplanation from "./components/landing-page/VideoExplanation";
 
 export default function Home() {
@@ -8,10 +9,11 @@ export default function Home() {
       <Header />
       <Hero />
       <VideoExplanation />
+      <Pricing />
       {/* 
       
       
-      <Pricing />
+      
       <FAQ /> */}
     </div>
   );
